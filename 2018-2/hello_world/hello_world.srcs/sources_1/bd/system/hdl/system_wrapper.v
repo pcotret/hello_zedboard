4866014b01e514d2bae4065b829d32fc7d5d4f59
Pascal <pascal@ensta-laptop>
2020-04-30 22:59:15 +0200

    Keep only essential project files. Remaining stuff is generated while the project is opened.

--- 2018-2/hello_world/hello_world.srcs/sources_1/bd/system/hdl/system_wrapper.v
+++ 2018-2/hello_world/hello_world.srcs/sources_1/bd/system/hdl/system_wrapper.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2018.2 (lin64) Build 2258646 Thu Jun 14 20:02:38 MDT 2018
-//Date        : Thu Apr 30 21:14:02 2020
+//Date        : Thu Apr 30 22:51:54 2020
 //Host        : ensta-laptop running 64-bit Ubuntu 20.04 LTS
 //Command     : generate_target system_wrapper.bd
 //Design      : system_wrapper
